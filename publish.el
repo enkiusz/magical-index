708ab052ffaf8c39aaafd36d1622453034541df8
Maciej Grela <enki@fsck.pl>
2023-01-11 16:56:00 +0100

    dlink-dir-655: Initial revision

--- publish.el
+++ publish.el
@@ -27,7 +27,7 @@
 
         ("magical-index-static"
          :base-directory "."
-         :base-extension "css\\|js\\|svg\\|pdf\\|bin\\|rpm\\|deb\\|png\\|webp\\|jpg\\|jpeg\\|gif\\|apk\\|tar.xz\\|tar.bz2\\|tar.gz\\|zip\\|txt\\|py\\|sh\\|config\\|kicad.*\\|logicdata\\|ino\\|csv\\|pcapng"
+         :base-extension "css\\|js\\|svg\\|pdf\\|bin\\|rpm\\|deb\\|png\\|webp\\|jpg\\|jpeg\\|gif\\|apk\\|tar.xz\\|tar.bz2\\|tar.gz\\|zip\\|txt\\|py\\|sh\\|config\\|kicad.*\\|logicdata\\|ino\\|csv\\|pcapng\\|sal"
          :exclude "_site/"
          :publishing-directory "_site/"
          :recursive t
